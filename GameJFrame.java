da3e9943168422d975f01c79593f959cd46ee27c
Maxlong18 <69859291+Maxlong18@users.noreply.github.com>
2020-08-19 13:59:18 +0800

    Update GameJFrame.java

--- GameJFrame.java
+++ GameJFrame.java
@@ -5,25 +5,33 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
+
+import gamejpanel.GameJPanel;
+
+
 public class GameJFrame extends JFrame{
 
-	private static final int DEFAULT_WIDTH = 600;
-	private static final int DEFAULT_HEIGHT = 800;
-	public static void main(String[] args) {
-		GameJFrame frame = new GameJFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		frame.setTitle("É³ÂÞÂüÉß");
-		frame.setVisible(true);
-	}
+	public static final int WIDTH = 600;
+	public static final int HEIGHT = 800;
+    private GameJPanel panel;
 	
 	public GameJFrame()
 	{
-		this.setSize(DEFAULT_WIDTH,DEFAULT_HEIGHT);
+		panel = new GameJPanel();
+		this.add(panel);
+		panel.action();
+		this.setSize(WIDTH,HEIGHT);
 		this.setResizable(false);
 	    this.setLocationByPlatform(true);
-	    
 	    Image img = new ImageIcon("image/fengmian.jpg").getImage();
         this.setIconImage(img);	
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.setTitle("æ²™ç½—æ›¼è›‡");
+		this.setVisible(true);
+	}
+	public static void main(String[] args) {
+		new GameJFrame();
+		
 	}
 }
